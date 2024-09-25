9aa2f0da7050928c62fddacbce0a066c8cdd3482
Aakash Vaithyanathan <aakash.vaithyanathan.2002@gmail.com>
2024-09-24 22:26:43 -0400

    Code to simulate TTC Streetcar delay data and tests for simulated data

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -1,19 +1,53 @@
 #### Preamble ####
-# Purpose: Simulates... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Simulates sample data for delay in TTC streetcars
+# Author: Aakash Vaithyanathan
+# Date: September 24, 2024
+# Contact: aakash.vaithyanathan@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: None
+# Any other information needed? None
 
 
 #### Workspace setup ####
 library(tidyverse)
-# [...UPDATE THIS...]
+set.seed(20136)
 
 #### Simulate data ####
-# [...ADD CODE HERE...]
+# Define the start and end date
+start_date <- as.Date("2023-01-01")
+end_date <- as.Date("2023-12-31")
+
+# Set the number of random dates you want to generate
+number_of_dates <- 100
+
+data <-
+  tibble(
+    dates = as.Date(
+      runif(
+        n = number_of_dates,
+        min = as.numeric(start_date),
+        max = as.numeric(end_date)
+      ),
+    ),
+    delay_mins = rpois(n = number_of_dates, lambda = 20),
+    day = weekdays(dates),
+    reason = sample(
+      c("Traffic", "Emergency", "Weather", "Route Change"), 
+      size = number_of_dates, replace = TRUE
+      ),
+    from_station = sample(
+      c("Finch", "Sheppard Yongue", "Lawrence Station", "Bloor Station"), 
+      size = number_of_dates, replace = TRUE
+    ),
+    to_station = sample(
+      c("St George Station", "Union Station", "Queen Station", "Dundas West", "Museum"), 
+      size = number_of_dates, replace = TRUE
+    ),
+  )
+
+
+#### Write_csv
+write_csv(data, file = "data/raw_data/simulated_streetcar_delay.csv")
 
 
 
--- scripts/03-test_data.R
+++ scripts/03-test_data.R
@@ -1,15 +1,29 @@
 #### Preamble ####
-# Purpose: Tests... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Tests whether simulated data contain valid entries or not
+# Author: Aakash Vaithyanathan
+# Date: September 24, 2024
+# Contact: aakash.vaithyanathan@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: None
+# Any other information needed? None
 
 
 #### Workspace setup ####
 library(tidyverse)
-# [...UPDATE THIS...]
+set.seed(20136)
 
 #### Test data ####
+data <- read_csv("data/raw_data/simulated_streetcar_delay.csv")
+
+# Test for negative numbers in delay_mins column
+data$delay_mins |> min() <= 0
+# Test for NAs in delay_mins column
+all(is.na(data$delay_mins))
+# Test for NAs in date column
+all(is.na(data$dates))
+# Test for NAs in reason column
+all(is.na(data$reason))
+# Test for NAs in to_station column
+all(is.na(data$to_station))
+# Test for NAs in from_station column
+all(is.na(data$from_station))
