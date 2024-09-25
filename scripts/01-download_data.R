3679d1802832156a872d356fca533a81173b81b9
Aakash Vaithyanathan <aakash.vaithyanathan.2002@gmail.com>
2024-09-24 23:28:25 -0400

    Add code to download data and perform data cleaning

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -1,26 +1,32 @@
 #### Preamble ####
-# Purpose: Downloads and saves the data from [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Downloads and saves the data from OpenToronto
+# Author: Aakash Vaithyanathan
+# Date: September 24, 2024
+# Contact: aakash.vaithyanathan@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: None
+# Any other information needed? None
 
 
 #### Workspace setup ####
 library(opendatatoronto)
 library(tidyverse)
-# [...UPDATE THIS...]
+set.seed(20136)
 
 #### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
+# get package
+package <- show_package("b68cb71b-44a7-4394-97e2-5d2f41462a5d")
 
+# get all resources for this package
+resources <- list_package_resources("b68cb71b-44a7-4394-97e2-5d2f41462a5d")
+datastore_resources <- filter(resources, tolower(format) == 'xlsx')
+
+# Load data for delays in the year of 2023
+delay_data_2023 <- filter(datastore_resources,
+                               name == "ttc-streetcar-delay-data-2023") %>% get_resource()
 
 
 #### Save data ####
-# [...UPDATE THIS...]
-# change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
+write_csv(delay_data_2023, "data/raw_data/unedited_ttc_streetcar_delay_2023.csv") 
 
          
--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -1,44 +1,33 @@
 #### Preamble ####
-# Purpose: Cleans the raw plane data recorded by two observers..... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 6 April 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Cleans the raw data into appropriate formats and removing missing values
+# Author: Aakash Vaithyanathan
+# Date: September 24, 2024
+# Contact: aakash.vaithyanathan@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: None
+# Any other information needed? None
 
 #### Workspace setup ####
 library(tidyverse)
+set.seed(20136)
 
 #### Clean data ####
-raw_data <- read_csv("inputs/data/plane_data.csv")
+raw_data <- read_csv("data/raw_data/unedited_ttc_streetcar_delay_2023.csv")
 
 cleaned_data <-
   raw_data |>
-  janitor::clean_names() |>
-  select(wing_width_mm, wing_length_mm, flying_time_sec_first_timer) |>
-  filter(wing_width_mm != "caw") |>
-  mutate(
-    flying_time_sec_first_timer = if_else(flying_time_sec_first_timer == "1,35",
-                                   "1.35",
-                                   flying_time_sec_first_timer)
-  ) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "490",
-                                 "49",
-                                 wing_width_mm)) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "6",
-                                 "60",
-                                 wing_width_mm)) |>
-  mutate(
-    wing_width_mm = as.numeric(wing_width_mm),
-    wing_length_mm = as.numeric(wing_length_mm),
-    flying_time_sec_first_timer = as.numeric(flying_time_sec_first_timer)
-  ) |>
-  rename(flying_time = flying_time_sec_first_timer,
-         width = wing_width_mm,
-         length = wing_length_mm
-         ) |> 
-  tidyr::drop_na()
+  janitor::clean_names() |> 
+  separate(col = date,
+           into = c("year", "month"),
+           sep = "-") |>
+  tidyr::drop_na() |>
+  filter(min_delay != 0) |>
+  filter(min_gap != 0) |>
+  mutate(time = substr(time, 1, 5)) |>
+  select(year, month, day, location, incident, min_delay, line)
+
+# Check if any column contains NA data or not
+any(is.na(cleaned_data))
 
 #### Save data ####
-write_csv(cleaned_data, "outputs/data/analysis_data.csv")
+write_csv(cleaned_data, "data/analysis_data/cleaned_ttc_streetcar_delay_2023.csv")
